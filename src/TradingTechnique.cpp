2fb97f4ebcdba69bf966431c59ef27489808eab4
AmeltonKhoza <u19201062@tuks.co.za>
2026-09-05 11:56:30 +0000

    Implement getFundBalance method in Trading techniques.cpp
    Added getFundBalance method to retrieve current fund balance.

--- src/TradingTechnique.cpp
+++ src/TradingTechnique.cpp
@@ -1,18 +1,21 @@
 #include "TradingTechnique.h"
 
-TradingTechnique::TradingTechnique(double startingBalance) : fundBalance(startingBalance) {}
+TradingTechnique::TradingTechnique(double startingBalance) : fundBalance(startingBalance) {
+	  this->fundBalance = startingBalance;
+}
 
 void TradingTechnique::onPriceUpdate(std::string ticker, double price)
 {
 	WorkerGroup::onPriceUpdate(ticker, price);
 }
 
-void TradingTechnique::subscribeAll()
-{
-	// need the stock market implementation first
-}
-
 void TradingTechnique::adjustBalance(double amount)
 {
-	fundBalance += amount;
+    fundBalance += amount;
+}
+
+
+double TradingTechnique::getFundBalance() const
+{
+    return fundBalance;
 }
