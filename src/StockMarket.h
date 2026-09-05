14ca9ef715ea5f3e399daca8918352714833a576
AmeltonKhoza <u19201062@tuks.co.za>
2026-09-05 01:28:51 +0000

    included the vector that stores all the subject subscribers and the notify and unsubscribe function
    Added include for MarketObserver and private member for observers.

--- src/StockMarket.h
+++ src/StockMarket.h
@@ -1,15 +1,16 @@
 #ifndef STOCKMARKET_H
 #define STOCKMARKET_H
-
+#include "MarketObserver.h"
 class StockMarket {
-
+private:
+std::vetor<MarketObserver*> children;
 
 public:
 	void getCurrentStock(String ticker);
-
 	double getStockPrice(String ticker);
-
 	void subscribe(MarketObserver observer, String ticker);
+	void subscribe(MarketObserver observer, String ticker);
+	void notify(std::string sticker, double price);
 };
 
 #endif
