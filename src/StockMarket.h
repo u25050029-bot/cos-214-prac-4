1911c4da549ee039224d2743cfa68d8798e965a1
AmeltonKhoza <u19201062@tuks.co.za>
2026-09-05 03:22:35 +0000

    Updated version of stockmarket.h file

--- src/StockMarket.h
+++ src/StockMarket.h
@@ -1,16 +1,27 @@
 #ifndef STOCKMARKET_H
 #define STOCKMARKET_H
+
+#include <string>
+#include <map>
+#include <vector>
+#include "MarketSubject.h"
 #include "MarketObserver.h"
-class StockMarket {
+
+class StockMarket : public MarketSubject
+{
 private:
-std::vetor<MarketObserver*> children;
+    std::map<std::string, std::vector<MarketObserver *>> observers;
+    std::map<std::string, double> prices;
 
 public:
-	void getCurrentStock(String ticker);
-	double getStockPrice(String ticker);
-	void subscribe(MarketObserver observer, String ticker);
-	void subscribe(MarketObserver observer, String ticker);
-	void notify(std::string sticker, double price);
+    void attach(MarketObserver *observer, std::string ticker) override;
+    void detach(MarketObserver *observer, std::string ticker) override;
+    void notify(std::string ticker, double price) override;
+
+    void setStockPrice(std::string ticker, double price);
+    double getStockPrice(std::string ticker);
+
+    void tick(std::string ticker, double price);
 };
 
 #endif
