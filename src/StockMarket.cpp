88bf827f1661898f9f37b889e55cd272eefa2566
AmeltonKhoza <u19201062@tuks.co.za>
2026-09-05 06:41:57 +0000

    implementation of the stockmarket.h file
    Implemented observer pattern methods for stock price updates.

--- src/StockMarket.cpp
+++ src/StockMarket.cpp
@@ -1,16 +1,48 @@
 #include "StockMarket.h"
+#include <algorithm>
 
-void StockMarket::getCurrentStock(String ticker) {
-	// TODO - implement StockMarket::getCurrentStock
-	throw "Not yet implemented";
+void StockMarket::attach(MarketObserver *observer, std::string ticker)
+{
+    observers[ticker].push_back(observer);
 }
 
-double StockMarket::getStockPrice(String ticker) {
-	// TODO - implement StockMarket::getStockPrice
-	throw "Not yet implemented";
+void StockMarket::detach(MarketObserver *observer, std::string ticker)
+{
+    std::map<std::string, std::vector<MarketObserver *>>::iterator it = observers.find(ticker);
+    if (it == observers.end())
+    {
+        return;
+    }
+    std::vector<MarketObserver *> &subs = it->second;
+    subs.erase(std::remove(subs.begin(), subs.end(), observer), subs.end());
 }
 
-void StockMarket::subscribe(MarketObserver observer, String ticker) {
-	// TODO - implement StockMarket::subscribe
-	throw "Not yet implemented";
+void StockMarket::notify(std::string ticker, double price)
+{
+    std::vector<MarketObserver *> &subs = observers[ticker];
+    for (std::size_t i = 0; i < subs.size(); ++i)
+    {
+        subs[i]->onPriceUpdate(ticker, price);
+    }
+}
+
+void StockMarket::setStockPrice(std::string ticker, double price)
+{
+    prices[ticker] = price;
+}
+
+double StockMarket::getStockPrice(std::string ticker)
+{
+    std::map<std::string, double>::iterator it = prices.find(ticker);
+    if (it == prices.end())
+    {
+        return 0.0;
+    }
+    return it->second;
+}
+
+void StockMarket::tick(std::string ticker, double price)
+{
+    prices[ticker] = price;
+    notify(ticker, price);
 }
