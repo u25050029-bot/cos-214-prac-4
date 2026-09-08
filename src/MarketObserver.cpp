81719aefabfa6c00f231d8e87d85c1d829fd065a
u25050029 <u25050029@tuks.co.za>
2026-09-08 05:46:46 +0200

    Removed Old MarketObserver file

--- src/MarketObserver.cpp
+++
@@ -1,6 +0,0 @@
-#include "MarketObserver.h"
-
-void MarketObserver::onPriceUpdate(String ticker, double price) {
-	// TODO - implement MarketObserver::onPriceUpdate
-	throw "Not yet implemented";
-}
